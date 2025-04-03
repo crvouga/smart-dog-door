907e0b2739ca1c6d1abd6b368286d1b07314dbf9
Chris Vouga <crvouga@gmail.com>
2025-04-02 21:41:28 -0700

    Refactor image resizing logic in `ImageClassifierTractOnnx`
    - Updated image resizing to use dynamic dimensions based on model configuration instead of hardcoded values, enhancing flexibility for different input shapes.
    - Simplified tensor creation logic to align with the new resizing approach, improving code clarity and maintainability.

--- src/image_classifier/impl_tract_onnx.rs
+++ src/image_classifier/impl_tract_onnx.rs
@@ -1,6 +1,6 @@
 use super::models::model_config::ModelConfig;
 use crate::image_classifier::interface::{Classification, ImageClassifier};
-use image::{DynamicImage, GenericImageView};
+use image::DynamicImage;
 use tract_onnx::prelude::*;
 
 pub struct ImageClassifierTractOnnx {
@@ -34,13 +34,16 @@ impl ImageClassifier for ImageClassifierTractOnnx {
         let mut results = Vec::new();
 
         for image in images {
-            let resized = image.resize_exact(224, 224, image::imageops::FilterType::Triangle);
+            let width = self.config.input_shape.0;
+            let height = self.config.input_shape.1;
+            let resized = image.resize_exact(width, height, image::imageops::FilterType::Triangle);
             let rgb = resized.to_rgb8();
-            let input_tensor: Tensor =
-                tract_ndarray::Array4::from_shape_fn((1, 3, 224, 224), |(_, c, y, x)| {
-                    rgb[(x as u32, y as u32)][c] as f32 / 255.0
-                })
-                .into();
+
+            let input_tensor: Tensor = tract_ndarray::Array4::from_shape_fn(
+                (1, 3, height as usize, width as usize),
+                |(_, c, y, x)| rgb[(x as u32, y as u32)][c] as f32 / 255.0,
+            )
+            .into();
 
             let output = self.model.run(tvec!(input_tensor.into()))?;
             let output_tensor = output[0].to_array_view::<f32>()?;
